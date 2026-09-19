81acdb33ac2e9192648b5beb8f6626975712ba90
reyjesusoviedo <santostrading618@gmail.com>
2026-09-19 15:52:17 +0200

    Add files via upload

--- release.sh
+++ release.sh
@@ -3,6 +3,7 @@
 V="1.0.$(date +%Y%m%d%H%M%S)"
 for f in *.html; do
   sed -i -E "s#(href=\"assets/[^\"?]+\.css)(\?v=[^\"]*)?\"#\1?v=$V\"#g; s#(src=\"assets/[^\"?]+\.js)(\?v=[^\"]*)?\"#\1?v=$V\"#g" "$f"
+  sed -i -E "s#var mine = \"[^\"]*\";#var mine = \"$V\";#" "$f"
 done
 sed -i -E "s/version: \"[^\"]*\"/version: \"$V\"/" assets/config.js
 echo "$V" > version.txt
