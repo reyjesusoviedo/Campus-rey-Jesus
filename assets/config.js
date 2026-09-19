a5a0db8dd6709c7c1d8f5348886c3b1d7ee76bff
reyjesusoviedo <santostrading618@gmail.com>
2026-09-19 15:54:52 +0200

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 // Configuración del campus · proyecto Supabase "campus"
 window.CAMPUS_CONFIG = {
-  version: "1.0.202609182142",
+  version: "1.0.20260919134705",
   url: "https://jkhpkoxcnefoptrmtzxv.supabase.co",
   key: "sb_publishable__zEKscajwJBJ3yB5Rdt1Uw_RpGn_AgD",
   brand: "Rey Jesús Oviedo",
